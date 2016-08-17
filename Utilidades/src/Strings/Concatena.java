e0a9c2cd04e8b3b17313bf930e3c5ceef5afa409
Luiz Felipe Mendes <felipe14_mendes@hotmail.com>
2016-08-17 15:06:07 -0300

    update
    adicionado laço de repetição para fazer com mais arquivos por vez

--- Utilidades/src/Strings/Concatena.java
+++ Utilidades/src/Strings/Concatena.java
@@ -22,33 +22,51 @@ public class Concatena {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String flag = "s";
 
-        System.out.println("Entre com o nome do arquivo: ");
-        String nome = input.nextLine();
-
-        System.out.println("Conteudo do arquivo texto: \n");
         try {
-            //Leitura de arquivo txt
-            FileReader arq = new FileReader("C:\\Users\\luiz_mendes\\Desktop\\JOBS\\17-08-2016\\" + nome + ".txt");
-            BufferedReader lerArq = new BufferedReader(arq);
-            
-            //Gravação em arquivo txt
-            FileWriter arq_ = new FileWriter("C:\\Users\\luiz_mendes\\Desktop\\JOBS\\17-08-2016\\resultado.txt");
-            PrintWriter gravarArq = new PrintWriter(arq_);
-            
-            String linha = lerArq.readLine();//Le a primeira linha, ao termino a variavel linha recebe null
-            while (linha != null) {
-               // System.out.println("UPDATE db_sinistro.dbo.SNS_AVISO_VIDA SET flg_idn_con = 'N' WHERE num_avs = '" + linha + "';");
-                gravarArq.printf("UPDATE db_sinistro.dbo.SNS_AVISO_VIDA SET flg_idn_con = 'N' WHERE num_avs = '%s'; %n", linha);
-                linha = lerArq.readLine();
+            while(flag.compareToIgnoreCase("s") == 0){
+
+                System.out.println("Entre com o ramo do arquivo: ");
+                String ramo = input.nextLine();
+
+                ///Leitura de arquivo txt
+                FileReader arq = new FileReader("C:\\Users\\luiz_mendes\\Desktop\\JOBS\\17-08-2016\\" + ramo + ".txt");
+                BufferedReader lerArq = new BufferedReader(arq);
+
+                //Gravação em arquivo txt
+                FileWriter arq_ = new FileWriter("C:\\Users\\luiz_mendes\\Desktop\\JOBS\\17-08-2016\\UPDATE_" + ramo.toUpperCase() + ".txt");
+                PrintWriter gravarArq = new PrintWriter(arq_);
+
+                String linha = lerArq.readLine();//Le a primeira linha, ao termino a variavel linha recebe null
+                while (linha != null) {
+                    // System.out.println("UPDATE db_sinistro.dbo.SNS_AVISO_VIDA SET flg_idn_con = 'N' WHERE num_avs = '" + linha + "';");
+                    switch (ramo) {
+                        case "vida":
+                            gravarArq.printf("UPDATE db_sinistro.dbo.SNS_AVISO_VIDA SET flg_idn_con = 'N' WHERE num_avs = '%s'; %n", linha);
+                            break;
+                        case "outro":
+                            gravarArq.printf("UPDATE db_sinistro.dbo.SNS_AVISO_DEMAIS_RAMOS SET flg_idn_con = 'N' WHERE num_avs = '%s'; %n", linha);
+                            break;
+                        case "auto":
+                            gravarArq.printf("UPDATE db_sinistro.dbo.SNS_AVISO_AUTO SET flg_idn_con = 'N' WHERE num_avs = '%s'; %n", linha);
+
+                    }
+
+                    linha = lerArq.readLine();
+                }
+
+                System.out.println("Deseja continuar? S/N ");
+                flag = input.nextLine();
+                arq_.close();
+
             }
-            
-            arq_.close();
 
         } catch (IOException e) {
             System.out.println("Erro na abertura do arquivo");
         }
         System.out.println("");
+
     }
 
 }
